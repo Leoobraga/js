3d18447ac7e6c9c08902bf1dd6508a07179ccf1e
leoobraga <leobraga.gudu@gmail.com>
2025-08-09 14:07:41 -0300

    estudando js

---
+++ ex010/scrip.js
@@ -0,0 +1,11 @@
+
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('foto')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML =` Agora são ${hora} horas `
+
+if ( hora >= )
+} 
+
